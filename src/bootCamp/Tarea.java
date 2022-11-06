cd54b3647aeefd0bf0ef450e3ad9a3a04bb84d75
Sdpuchatt <sdpuchatt@gmail.com>
2022-11-06 12:46:17 -0300

    Ejercicio 3

--- src/bootCamp/Tarea.java
+++ src/bootCamp/Tarea.java
@@ -17,23 +17,6 @@ public class Tarea {
 		return a + b + c;
 	}
 	
-	
-	
-	/*
-	
-		
-		Segunda parte:
-		
-		Crear una clase coche.
-		
-		Dentro de la clase coche, una variable numérica que almacene el número de puertas que tiene.
-		
-		Una función que incremente el número de puertas que tiene el coche.
-		
-		Crear un objeto miCoche en el main y añadirle una puerta.
-		
-		Mostrar el número de puertas que tiene el objeto.
-	 */
 }
 class Coche{
 	public int Cantidadpuertas = 0;
