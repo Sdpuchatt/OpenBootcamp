b421d5c1f1e8361f884636a0bdc692b492c360bb
Sdpuchatt <sdpuchatt@gmail.com>
2022-11-15 19:43:45 -0300

    Realice clase trabajador

--- src/bootcampHerencia/EjercicioTema9.java
+++ src/bootcampHerencia/EjercicioTema9.java
@@ -3,10 +3,15 @@ package bootcampHerencia;
 public class EjercicioTema9 {
 	public static void main(String[] args) {
 		Cliente cliente1 = new Cliente(33, "Sergio", 4782419, 1000);
-		System.out.println("Tengo " + cliente1.getEdad());
+		System.out.println("Soy un cliente, tengo " + cliente1.getEdad());
 		System.out.println("Me llamo "+cliente1.getNombre());
 		System.out.println("Mi telefono es "+ cliente1.getTelefono());
 		System.out.println("Tengo "+ cliente1.getCredito()+" de credito");
+		System.out.println("--------------------------");
+		
+		Trabajador t1 = new Trabajador(33, "Daniel", 4783555, 200000);
+		System.out.println("Soy un trabajador tengo "+t1.getEdad() +"\n Mi nombre es "+ t1.getNombre() 
+		+"\n Telefono: " + t1.getTelefono() + "\n mi salario "+ t1.getSalario());
 	}
 }
 
@@ -42,5 +47,14 @@ class Cliente extends Persona{
 	public int getCredito () {
 		return this.credito;
 	}
-
+}
+class Trabajador extends Persona{
+	private int salario;
+	public Trabajador(int edad, String nombre, int telefono, int salario) {
+		super(edad, nombre, telefono);
+		this.salario = salario;
+	}
+	public int getSalario() {
+		return this.salario;
+	}
 }
