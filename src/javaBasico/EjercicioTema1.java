9c9ad6e4311a0dbb487c03a414eb990ec265874d
Sdpuchatt <sdpuchatt@gmail.com>
2022-11-21 00:20:54 -0300

    Primer ejercicio del curso java Basico

---
+++ src/javaBasico/EjercicioTema1.java
@@ -0,0 +1,18 @@
+package javaBasico;
+
+public class EjercicioTema1 {
+	public static void main(String[] args) {
+		//int, long, double, boolean, String
+		int numeroEntero = 10;
+		long numeroLong = 25000L;
+		double numDecimal = 10.5d;
+		boolean bandera = true;
+		String nombre = "Sergio";
+		
+		System.out.println(numeroEntero);
+		System.out.println(numeroLong);
+		System.out.println(numDecimal);
+		System.out.println(bandera);
+		System.out.println(nombre);
+	}
+}
